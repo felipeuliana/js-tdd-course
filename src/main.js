9c074f4d62f6ed0982ee0ced7ae9de5d443b72de
Felipe Uliana <felipevendramin@ext.cvc.com.br>
2019-03-29 08:59:11 -0300

    Add 'AlbumInfo' component to the app

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import spotify from './Spotify';
 import renderAlbums from './AlbumList';
+import renderAlbumInfo from './AlbumInfo';
 
 const albums = spotify.search.albums('Deftones');
 const albumList = document.getElementById('album-list');
 
+const album = spotify.album.getAlbum('5LEXck3kfixFaA3CqVE7bC');
+const albumInfo = document.getElementById('album-info');
+
 albums.then((data) => renderAlbums(data.albums.items, albumList));
+album.then((data) => renderAlbumInfo(data, albumInfo));
