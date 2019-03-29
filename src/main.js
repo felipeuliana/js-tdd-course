607427549ef05cbda7f36d69b8facdf1ed51de63
Felipe Uliana <felipevendramin@ext.cvc.com.br>
2019-03-29 08:25:57 -0300

    Add 'AlbumList' component to the app

--- src/main.js
+++ src/main.js
@@ -1,1 +1,7 @@
-console.log('Spotify Wrapper Player is up!');
+import spotify from './Spotify';
+import renderAlbums from './AlbumList';
+
+const albums = spotify.search.albums('Deftones');
+const albumList = document.getElementById('album-list');
+
+albums.then((data) => renderAlbums(data.albums.items, albumList));
